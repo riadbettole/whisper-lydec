c171cc0d870675db95e3a0013126aa9a43300f67
riadbettole <riadbettole@gmail.com>
2023-09-25 00:26:11 +0100

    first

--- example/urls.py
+++ example/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 
 from example.views import index
+from example.views import save_audio
 
 
 urlpatterns = [
-    path('', index),
+    path('save_transcription/', save_audio, name='save_audio1'),
+    path('', index, name='save_audio2'),
 ]
--- example/views.py
+++ example/views.py
@@ -1,16 +1,37 @@
 # example/views.py
-from datetime import datetime
 
-from django.http import HttpResponse
+import openai
+from django.shortcuts import render
+from django.http import JsonResponse
+import os
+
+openai.api_key = os.environ.get('api_key')
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, 'index.html')
+
+def save_audio(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+
+        audio_file = request.FILES['audio_file']
+
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+        system_msg = " here are two transcript of the user, look at the one that makes sense more and do what it asks"
+        user_msg = "first transcript: " + text + "; second transcript: " + transcript.text
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature = 0.6,
+        max_tokens = 50,
+        messages=[{"role": "system", "content": system_msg},
+                        {"role": "user", "content": user_msg}])
+        
+        response2 = response.choices[0]
+
+        response3 = response2.message
+
+        response4 = response3.content
+
+        return JsonResponse({'id': response4})
+    return JsonResponse({'error': 'Invalid request method'})
